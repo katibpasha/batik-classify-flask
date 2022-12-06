7429661f3e08b7c6293d5a94794ff8f6c06496e0
titan.bramantyo <titan.bramantyo@students.amikom.ac.id>
2022-12-07 01:38:42 +0700

    prediction result parsing still error

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, flash, request, redirect, url_for, render_template
+from importlib.resources import path
 import os
 from werkzeug.utils import secure_filename
 import pickle
 from skimage.feature import local_binary_pattern
 import numpy as np
-import pandas as pd
+import cv2 as cv
 
 app = Flask(__name__)
 
@@ -31,16 +32,15 @@ METHOD = 'uniform'
 # make function of LBP
 
 
-def local_binary(img):
+def predict_label(img):
     img_lbp = local_binary_pattern(img, n_points, radius, METHOD)
     img_lbp_hist, bins = np.histogram(img_lbp.ravel(), 256, [0, 256])
     img_lbp_hist = np.transpose(img_lbp_hist[0:256, np.newaxis])
-    return img_lbp_hist
 
-
-def predict_label(img):
-    knn = model_knn.predict(img)
-    return knn
+    knn = model_knn.predict(img_lbp_hist)
+    svc = model_svc.predict(img_lbp_hist)
+    nb = model_nb.predict(img_lbp_hist)
+    return knn, svc, nb
 
 
 @app.route('/')
@@ -48,30 +48,26 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
         flash('Error apa ini ya?')
         return redirect(request.url)
+
     file = request.files['file']
+
     if file.filename == '':
         flash('Tidak ada gambar yang diunggah')
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
         #print('upload_image filename: ' + filename)
         return render_template('index.html', filename=filename)
     else:
         flash('Ekstensi file yang diterima adalah JPG/JPEG')
         return redirect(request.url)
-
-
-def predict():
-    file = request.files['file']
-    x = local_binary(file)
-    label_output = predict_label(x)
-    return render_template('index.html', prediction=label_output)
 
 
 @app.route('/display/<filename>')
